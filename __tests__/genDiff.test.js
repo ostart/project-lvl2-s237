fbf009e512bd38dead15175ab32869033680435e
Artem Osta <ostart@mail.ru>
2018-04-06 16:47:54 +0300

    Task6 plain renderer for new AST has done

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,141 +1,142 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('compare two simple json config files', () => {
-  const path1 = './__tests__/__fixtures__/before.json';
-  const path2 = './__tests__/__fixtures__/after.json';
-  const resultPath = './__tests__/__fixtures__/compareResult.txt';
+describe('Simple files', () => {
+  test('compare two simple json config files', () => {
+    const path1 = './__tests__/__fixtures__/before.json';
+    const path2 = './__tests__/__fixtures__/after.json';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two simple yaml config files', () => {
+    const path1 = './__tests__/__fixtures__/before.yml';
+    const path2 = './__tests__/__fixtures__/after.yml';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two simple ini config files', () => {
+    const path1 = './__tests__/__fixtures__/before.ini';
+    const path2 = './__tests__/__fixtures__/after.ini';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
 });
 
-test('compare two simple yaml config files', () => {
-  const path1 = './__tests__/__fixtures__/before.yml';
-  const path2 = './__tests__/__fixtures__/after.yml';
-  const resultPath = './__tests__/__fixtures__/compareResult.txt';
+describe('Complicated files Default output format', () => {
+  test('compare two complicated json config files', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.json';
+    const path2 = './__tests__/__fixtures__/afterComplex.json';
+    const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two complicated yaml config files', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.yml';
+    const path2 = './__tests__/__fixtures__/afterComplex.yml';
+    const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two complicated ini config files', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.ini';
+    const path2 = './__tests__/__fixtures__/afterComplex.ini';
+    const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
 });
 
-test('compare two simple ini config files', () => {
-  const path1 = './__tests__/__fixtures__/before.ini';
-  const path2 = './__tests__/__fixtures__/after.ini';
-  const resultPath = './__tests__/__fixtures__/compareResult.txt';
+describe('Complicated files Plain output format', () => {
+  test('compare two complicated json config files in plain format', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.json';
+    const path2 = './__tests__/__fixtures__/afterComplex.json';
+    const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+    const diff = genDiff(path1, path2, 'plain');
+    const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two complicated yaml config files in plain format', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.yml';
+    const path2 = './__tests__/__fixtures__/afterComplex.yml';
+    const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
+
+    const diff = genDiff(path1, path2, 'plain');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two complicated ini config files in plain format', () => {
+    const path1 = './__tests__/__fixtures__/beforeComplex.ini';
+    const path2 = './__tests__/__fixtures__/afterComplex.ini';
+    const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
+
+    const diff = genDiff(path1, path2, 'plain');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
 });
 
-test('compare two complicated json config files', () => {
-  const path1 = './__tests__/__fixtures__/beforeComplex.json';
-  const path2 = './__tests__/__fixtures__/afterComplex.json';
-  const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
+// describe('Complicated files Json output format', () => {
+//   test('compare two complicated json config files in json format', () => {
+//     const path1 = './__tests__/__fixtures__/beforeComplex.json';
+//     const path2 = './__tests__/__fixtures__/afterComplex.json';
+//     const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+//     const diff = genDiff(path1, path2, 'json');
+//     const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
-});
+//     expect(diff).toEqual(result);
+//   });
 
-test('compare two complicated yaml config files', () => {
-  const path1 = './__tests__/__fixtures__/beforeComplex.yml';
-  const path2 = './__tests__/__fixtures__/afterComplex.yml';
-  const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
+//   test('compare two complicated yaml config files in json format', () => {
+//     const path1 = './__tests__/__fixtures__/beforeComplex.yml';
+//     const path2 = './__tests__/__fixtures__/afterComplex.yml';
+//     const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+//     const diff = genDiff(path1, path2, 'json');
+//     const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
-});
+//     expect(diff).toEqual(result);
+//   });
 
-test('compare two complicated ini config files', () => {
-  const path1 = './__tests__/__fixtures__/beforeComplex.ini';
-  const path2 = './__tests__/__fixtures__/afterComplex.ini';
-  const resultPath = './__tests__/__fixtures__/compareResultComplex.txt';
+//   test('compare two complicated ini config files in json format', () => {
+//     const path1 = './__tests__/__fixtures__/beforeComplex.ini';
+//     const path2 = './__tests__/__fixtures__/afterComplex.ini';
+//     const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
 
-  const diff = genDiff(path1, path2);
-  const result = fs.readFileSync(resultPath, 'utf-8');
+//     const diff = genDiff(path1, path2, 'json');
+//     const result = fs.readFileSync(resultPath, 'utf-8');
 
-  expect(diff).toEqual(result);
-});
-
-// test('compare two complicated json config files in plain format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.json';
-//   const path2 = './__tests__/__fixtures__/afterComplex.json';
-//   const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
-
-//   const diff = genDiff(path1, path2, 'plain');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
+//     expect(diff).toEqual(result);
+//   });
 // });
-
-// test('compare two complicated yaml config files in plain format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.yml';
-//   const path2 = './__tests__/__fixtures__/afterComplex.yml';
-//   const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
-
-//   const diff = genDiff(path1, path2, 'plain');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
-// });
-
-// test('compare two complicated ini config files in plain format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.ini';
-//   const path2 = './__tests__/__fixtures__/afterComplex.ini';
-//   const resultPath = './__tests__/__fixtures__/comparePlainComplex.txt';
-
-//   const diff = genDiff(path1, path2, 'plain');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
-// });
-
-// test('compare two complicated json config files in json format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.json';
-//   const path2 = './__tests__/__fixtures__/afterComplex.json';
-//   const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
-
-//   const diff = genDiff(path1, path2, 'json');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
-// });
-
-// test('compare two complicated yaml config files in jsonn format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.yml';
-//   const path2 = './__tests__/__fixtures__/afterComplex.yml';
-//   const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
-
-//   const diff = genDiff(path1, path2, 'json');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
-// });
-
-// test('compare two complicated ini config files in json format', () => {
-//   const path1 = './__tests__/__fixtures__/beforeComplex.ini';
-//   const path2 = './__tests__/__fixtures__/afterComplex.ini';
-//   const resultPath = './__tests__/__fixtures__/compareJsonComplex.json';
-
-//   const diff = genDiff(path1, path2, 'json');
-//   const result = fs.readFileSync(resultPath, 'utf-8');
-
-//   expect(diff).toEqual(result);
-// });
-
-
-// const path1 = './__tests__/__fixtures__/beforeComplex.json';
-// const path2 = './__tests__/__fixtures__/afterComplex.json';
-
-// const diff = genDiff(path1, path2);
-// console.log(diff);
--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -1,28 +1,31 @@
+const stringify = obj => (obj instanceof Object ? 'complex value' : `'${obj}'`);
+
 const renderPlainFromAst = (arr, keyPathArr = []) => {
-  const outStr = arr.reduce((acc, [state, key, ...rest]) => {
-    const [value] = rest;
+  const outStr = arr.reduce((acc, obj) => {
+    const {
+      key, valBefore, valAfter, children,
+    } = obj;
     const keyPath = keyPathArr.concat(key).join('.');
-    switch (state) {
-      case 'add':
-        return (value instanceof Array) ?
-          acc.concat(`Property '${keyPath}' was added with complex value\n`) :
-          acc.concat(`Property '${keyPath}' was added with value: '${value}'\n`);
-      case 'del':
-        return acc.concat(`Property '${keyPath}' was removed\n`);
-      case 'upd': {
-        const [valBefore, valAfter] = rest;
-        const before = (valBefore instanceof Array) ? 'complex value' : `'${valBefore}'`;
-        const after = (valAfter instanceof Array) ? 'complex value' : `'${valAfter}'`;
-        return acc.concat(`Property '${keyPath}' was updated. From ${before} to ${after}\n`);
+    switch (obj.type) {
+      case 'added':
+        return (valAfter instanceof Object) ?
+          [...acc, `Property '${keyPath}' was added with complex value`] :
+          [...acc, `Property '${keyPath}' was added with value: '${valAfter}'`];
+      case 'deleted':
+        return [...acc, `Property '${keyPath}' was removed`];
+      case 'updated': {
+        const before = stringify(valBefore);
+        const after = stringify(valAfter);
+        return [...acc, `Property '${keyPath}' was updated. From ${before} to ${after}`];
       }
-      case 'fix':
-        return (value instanceof Array) ?
-          acc.concat(renderPlainFromAst(value, keyPathArr.concat(key))) :
+      case 'fixed':
+        return (children) ?
+          [...acc, renderPlainFromAst(children, keyPathArr.concat(key))] :
           acc;
       default:
         return acc;
     }
-  }, '');
+  }, []).join('\n');
   return outStr;
 };
 
